6c62d50ce971bb85cd5b456f607094059b6b1b54
Imran Haider <ihaider@gmx.com>
2021-07-24 01:48:03 +0530

    Update rotate-date-stamped-logs.sh

--- rotate-date-stamped-logs.sh
+++ rotate-date-stamped-logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Logrotate doesn't play nice with files that have timestamps in filenames, so needed to use this script for log rotation. you can place this inside /etc/cron.daily/ or /etc/cron.weekly/.
+#Logrotate doesn't play nice with files that have timestamps in filenames, so needed to use this script for log rotation. You can place this inside /etc/cron.daily/ or /etc/cron.weekly/.
 find /var/log/remote/* -mtime +7 -delete;
 find /var/log/remote/* -maxdepth 1 -type f -name "*.log" -mtime +1 -exec gzip -9 {} \;
