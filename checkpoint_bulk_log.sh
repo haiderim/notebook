7f1f1154e1ccf787c310848233dd5cffac63b138
Imran Haider <ihaider@gmx.com>
2023-02-01 15:21:46 +0530

    Update checkpoint_bulk_log.sh

--- checkpoint_bulk_log.sh
+++ checkpoint_bulk_log.sh
@@ -2,9 +2,9 @@
 #The script changes logging settings in Firewall Network policy layer to enable firewall sessions and disable connection logging, you'd need to publish the changes manually. Publish wasn't included as you'd ideally want to review the changes and then publish them manually.
 mgmt_cli login -r true > id.txt
 echo "Enter total number of rules in policy"
-read END
+read limit
 echo "Enter policy layer name"
 read layername
-for ((i=1;i<=END;i++)); do
+for ((i=1;i<=limit;i++)); do
 mgmt_cli set access-rule rule-number $i track.per-session true track.per-connection false track.enable-firewall-session true track.type "Detailed Log" layer "$layername" -s id.txt
 done
