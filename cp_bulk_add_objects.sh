d02842ce2ae5605bc749cb7aa0acc265fcfb9870
Imran Haider <ihaider@gmx.com>
2023-09-12 11:07:20 +0530

    Create cp_bulk_add_objects.sh

---
+++ cp_bulk_add_objects.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#This script adds host objects present in addresses.txt file and publishes changes, you need to create the file with each line containing new object 
+mgmt_cli login -r true > id.txt
+input="addresses.txt"
+while IFS= read -r line
+do
+  mgmt_cli add host name "$line" ip-address "$line" groups groupname -s id.txt
+  mgmt_cli publish -s id.txt
+done < "$input"
